6d0a0a84cc8beaac338bc35673a90d0738bce91a
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-07 20:39:26 -0700

    can type in all of the fields and verified that text is in some of them. I have also verified that the button is there. I discovered I needed to add an id to the elements to get them by label text.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,58 +1,71 @@
 import React from "react";
-import { render, fireEvent, getAllByLabelText } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
-test("renders App without crashing", () => {
+test("renders contact form without crashing", () => {
     render(<ContactForm />);
 });
 
-test("name feilds are present and can type in them", () => {
-    const { getByPlaceholderText, getByText, getByLabelText } = render(<ContactForm />);
+test("first name field is present and can type in it with new text displayed", () => {
+    const { getByPlaceholderText, getByDisplayValue } = render(<ContactForm />);
 
     const firstNameInput = getByPlaceholderText(/Edd/i);
-    const lastNameInput = getByPlaceholderText(/Burke/i);
 
     fireEvent.change(firstNameInput, { target: { value: "Jojojo" } });
-    fireEvent.change(lastNameInput, { target: { value: "Potato" } });
+    const firstNameContent = getByDisplayValue(/Jojojo/i);
 
     expect(firstNameInput).toBeInTheDocument();
+    expect(firstNameContent).toBeInTheDocument();
+});
+test("last name field is present and can type in it with new text displayed", () => {
+    const { getByPlaceholderText, getByDisplayValue } = render(<ContactForm />);
+
+    const lastNameInput = getByPlaceholderText(/Burke/i);
+
+    fireEvent.change(lastNameInput, { target: { value: "Potato" } });
+    const lastNameContent = getByDisplayValue(/Potato/i);
+
     expect(lastNameInput).toBeInTheDocument();
+    expect(lastNameContent).toBeInTheDocument();
 });
 
-test("error messages exist", () => {
-    const { getByText, getByPlaceholderText } = render(<ContactForm />);
-    const firstNameInput = getByPlaceholderText(/Edd/i);
-    const firstNameError = getByText("Looks like there was an error:");
-    fireEvent.change(firstNameInput, { target: { value: "Jojojo" } });
-    expect(firstNameError).toBeInTheDocument();
-
-})
+// test("error messages exist", () => {
+//     const { getByText, getByPlaceholderText } = render(<ContactForm />);
+//     const firstNameInput = getByPlaceholderText(/Edd/i);
+//     fireEvent.change(firstNameInput, { target: { value: "Jojojo" } });
+//     const lastNameInput = getByPlaceholderText(/Burke/i);
+//     fireEvent.change(lastNameInput, { target: { value: "Potato" } });
+//     const firstNameError = getByText("maxLength");
+//     expect(firstNameError).toBeInTheDocument();
+// });
 
 test("email is present and can be typed into", () => {
-    const { getByPlaceholderText, getByText, getByLabelText } = render(<ContactForm />);
+    const { getByLabelText, getByDisplayValue } = render(<ContactForm />);
 
 
-    const emailInput = getByPlaceholderText(/bluebill1049@hotmail.com/i);
+    const emailInput = getByLabelText(/email/i);
 
-    // fireEvent.change(emailInput, { target: { value: "a@b.c" } });
+    fireEvent.change(emailInput, { target: { value: "a@b.c" } });
+    const emailContent = getByDisplayValue(/a@b.c/i);
 
     expect(emailInput).toBeInTheDocument();
-
+    expect(emailContent).toBeInTheDocument();
 });
 
 test("textArea is present and can be typed into", () => {
-    const { getByPlaceholderText, getByText, getByLabelText } = render(<ContactForm />);
-    const messageInput = getByText(/Message/i);
+    const { getByLabelText } = render(<ContactForm />);
 
-    // fireEvent.change(messageInput, { target: { value: "Greetings textArea" } });
+    const messageInput = getByLabelText(/Message/i);
+
+    fireEvent.change(messageInput, { target: { value: "Greetings textArea" } });
 
     expect(messageInput).toBeInTheDocument();
 
 });
-// test("button is present and can be used", () => {
-//     const { getByRole } = render(<ContactForm />);
+test("button is present and can be used", () => {
+    const { getByTestId } = render(<ContactForm />);
 
-//     const button = getByRole('button', {name: /submit!/i});
+    const button = getByTestId(/submit-button/i);
 
-//     expect(button).toBeInTheDocument();
-// });
+    expect(button).toBeInTheDocument();
+});
